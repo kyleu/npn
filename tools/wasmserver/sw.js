9e02307390c1c50167f1773f4f9a8b743114df3a
Kyle Unverferth <kyle@kyleu.com>
2023-10-14 16:36:32 -0600

    Nav, Playwright improvements

--- tools/wasmserver/sw.js
+++ tools/wasmserver/sw.js
@@ -97,7 +97,7 @@ self.addEventListener('message', (event) => {
 });
 
 function isLocal(url) {
-  if (url.toString().startsWith("https://github.com/kyleu/npn")) {
+  if (url.toString().startsWith("https://github.com/kyleu/npn/")) {
     return true;
   }
   for (const s of ["favicon.ico", "logo.svg", "npn.wasm", "server.js", "sw.js", "wasm_exec.js"]) {
