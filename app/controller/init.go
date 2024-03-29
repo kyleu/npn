7c90ae0e983252c685bc4e951181f07e353d26fb
Kyle Unverferth <kyle@kyleu.com>
2024-03-29 12:33:32 -0600

    Notarization and initialization improvments

--- app/controller/init.go
+++ app/controller/init.go
@@ -1,13 +1,16 @@
 package controller
 
 import (
+	"context"
+
 	"github.com/kyleu/npn/app"
 	"github.com/kyleu/npn/app/controller/cutil"
 	"github.com/kyleu/npn/app/util"
 )
 
 // Initialize app-specific system dependencies.
-func initApp(_ *app.State, _ util.Logger) {
+func initApp(_ context.Context, _ *app.State, _ util.Logger) error {
+	return nil
 }
 
 // Configure app-specific data for each request.
--- app/controller/siteinit.go
+++ app/controller/siteinit.go
@@ -1,13 +1,16 @@
 package controller
 
 import (
+	"context"
+
 	"github.com/kyleu/npn/app"
 	"github.com/kyleu/npn/app/controller/cutil"
 	"github.com/kyleu/npn/app/util"
 )
 
 // Initialize system dependencies for the marketing site.
-func initSite(*app.State, util.Logger) {
+func initSite(_ context.Context, _ *app.State, _ util.Logger) error {
+	return nil
 }
 
 // Configure marketing site data for each request.
