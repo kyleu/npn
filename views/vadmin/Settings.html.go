2cb53fa341949340e9cd1546caa4d7b9f2786c60
Kyle Unverferth <kyle@kyleu.com>
2024-06-15 11:17:18 -0600

    Large refactors, new utility methods, linting

--- views/vadmin/Settings.html.go
+++ views/vadmin/Settings.html.go
@@ -62,6 +62,8 @@ func (p *Settings) StreamBody(qw422016 *qt422016.Writer, as *app.State, ps *cuti
 //line views/vadmin/Settings.html:22
 	components.StreamSVGIcon(qw422016, `cog`, ps)
 //line views/vadmin/Settings.html:22
+	qw422016.N().S(` `)
+//line views/vadmin/Settings.html:22
 	qw422016.E().S(util.AppName)
 //line views/vadmin/Settings.html:22
 	qw422016.N().S(` `)
@@ -105,7 +107,7 @@ func (p *Settings) StreamBody(qw422016 *qt422016.Writer, as *app.State, ps *cuti
 //line views/vadmin/Settings.html:33
 	components.StreamSVGIcon(qw422016, `archive`, ps)
 //line views/vadmin/Settings.html:33
-	qw422016.N().S(`Admin Functions</h3>
+	qw422016.N().S(` Admin Functions</h3>
     `)
 //line views/vadmin/Settings.html:34
 	streamsettingsLink(qw422016, "/admin/server", "archive", "App Information", "All sorts of info about the server and runtime", ps)
@@ -138,7 +140,7 @@ func (p *Settings) StreamBody(qw422016 *qt422016.Writer, as *app.State, ps *cuti
 //line views/vadmin/Settings.html:42
 	components.StreamSVGIcon(qw422016, `bolt`, ps)
 //line views/vadmin/Settings.html:42
-	qw422016.N().S(`HTTP Methods</h3>
+	qw422016.N().S(` HTTP Methods</h3>
     `)
 //line views/vadmin/Settings.html:43
 	streamsettingsLink(qw422016, "/admin/sitemap", "bolt", "Sitemap", "Displays the HTTP actions that are available, with documentation", ps)
@@ -171,7 +173,7 @@ func (p *Settings) StreamBody(qw422016 *qt422016.Writer, as *app.State, ps *cuti
 //line views/vadmin/Settings.html:51
 	components.StreamSVGIcon(qw422016, `cog`, ps)
 //line views/vadmin/Settings.html:51
-	qw422016.N().S(`App Profiling</h3>
+	qw422016.N().S(` App Profiling</h3>
     `)
 //line views/vadmin/Settings.html:52
 	streamsettingsLink(qw422016, "/admin/memusage", "cog", "Memory Usage", "Detailed memory usage statistics for this application", ps)
