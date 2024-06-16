f6f679e91e88d794882f9583b520fe71edab0ae0
Kyle Unverferth <kyle@kyleu.com>
2024-06-16 17:17:46 -0600

    Menu generation telemetry

--- app/controller/cmenu/menu.go
+++ app/controller/cmenu/menu.go
@@ -7,6 +7,7 @@ import (
 	"github.com/kyleu/npn/app"
 	"github.com/kyleu/npn/app/lib/filter"
 	"github.com/kyleu/npn/app/lib/menu"
+	"github.com/kyleu/npn/app/lib/telemetry"
 	"github.com/kyleu/npn/app/lib/user"
 	"github.com/kyleu/npn/app/util"
 )
@@ -14,6 +15,8 @@ import (
 func MenuFor(
 	ctx context.Context, isAuthed bool, isAdmin bool, profile *user.Profile, params filter.ParamSet, as *app.State, logger util.Logger, //nolint:revive
 ) (menu.Items, any, error) {
+	ctx, sp, _ := telemetry.StartSpan(ctx, "menu", logger)
+	defer sp.Complete()
 	var ret menu.Items
 	var data any
 	// $PF_SECTION_START(menu)$
