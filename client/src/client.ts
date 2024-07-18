4114474baca6310dae62badf44e9cd8979b3cb2b
Kyle Unverferth <kyle@kyleu.com>
2024-07-18 13:15:58 -0600

    TypeScript enhancements, upgrades, and linting

--- client/src/client.ts
+++ client/src/client.ts
@@ -14,7 +14,7 @@ import {socketInit} from "./socket";
 import {appInit} from "./app";
 
 declare global {
-  interface Window { // eslint-disable-line @typescript-eslint/consistent-type-definitions
+  interface Window {
     "npn": {
       wireTime: (el: HTMLElement) => void;
       relativeTime: (el: HTMLElement) => string;
