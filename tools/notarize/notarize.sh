8a816128129f86165b4e65242af2d8d70da4bd46
Kyle Unverferth <kyle@kyleu.com>
2025-01-05 14:10:07 -0700

    v0.2.0

--- tools/notarize/notarize.sh
+++ tools/notarize/notarize.sh
@@ -6,7 +6,7 @@ cd "$dir/../.."
 
 if [ "$PUBLISH_TEST" != "true" ]
 then
-  xcrun notarytool submit --apple-id $APPLE_EMAIL --team-id $APPLE_TEAM_ID --password $APPLE_PASSWORD ./build/dist/npn_0.1.44_darwin_amd64_desktop.dmg
-  xcrun notarytool submit --apple-id $APPLE_EMAIL --team-id $APPLE_TEAM_ID --password $APPLE_PASSWORD ./build/dist/npn_0.1.44_darwin_arm64_desktop.dmg
-  xcrun notarytool submit --apple-id $APPLE_EMAIL --team-id $APPLE_TEAM_ID --password $APPLE_PASSWORD ./build/dist/npn_0.1.44_darwin_all_desktop.dmg
+  xcrun notarytool submit --apple-id $APPLE_EMAIL --team-id $APPLE_TEAM_ID --password $APPLE_PASSWORD ./build/dist/npn_0.2.0_darwin_amd64_desktop.dmg
+  xcrun notarytool submit --apple-id $APPLE_EMAIL --team-id $APPLE_TEAM_ID --password $APPLE_PASSWORD ./build/dist/npn_0.2.0_darwin_arm64_desktop.dmg
+  xcrun notarytool submit --apple-id $APPLE_EMAIL --team-id $APPLE_TEAM_ID --password $APPLE_PASSWORD ./build/dist/npn_0.2.0_darwin_all_desktop.dmg
 fi
